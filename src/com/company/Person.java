3fca05e63c685e76c2072752abc88a290a6dbfe2
jack <kokolubilubi@gmail.com>
2020-12-16 19:17:22 +0100

    Constructor and getters and setters are added.

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,4 +1,37 @@
 package com.company;
 
 public class Person {
+    private int Age;
+    private String Name;
+    private String Address;
+
+    public Person(int age, String name, String address) {
+        Age = age;
+        Name = name;
+        Address = address;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
 }
