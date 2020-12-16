81625f9895edfacdd1e76b9c0bb3246d8ec36ed3
jack <kokolubilubi@gmail.com>
2020-12-16 21:51:32 +0100

    merge should happen!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
         // write your code here
         System.out.println("Koko Lubilubi is better! than gholibeigian");
         System.out.printf("It is printf function!");
-        System.out.println("Gholibeigian(Mary) is back!");
+        System.out.println("22");
     }
 }
