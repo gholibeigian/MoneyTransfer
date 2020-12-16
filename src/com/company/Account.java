896a582657213d286de3de29874e32110c15519d
jack <kokolubilubi@gmail.com>
2020-12-16 21:18:03 +0100

    This is Koko. Tada!

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -2,4 +2,28 @@ package com.company;
 
 public class Account {
     ///test2
+    // We do not need test here. We need action!
+    private int AccountNumber;
+    private String AccountType;
+
+    public Account(int accountNumber, String accountType) {
+        AccountNumber = accountNumber;
+        AccountType = accountType;
+    }
+
+    public int getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        AccountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return AccountType;
+    }
+
+    public void setAccountType(String accountType) {
+        AccountType = accountType;
+    }
 }
