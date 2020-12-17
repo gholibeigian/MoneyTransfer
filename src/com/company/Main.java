2f561fc014567cd73fea9c8659f6f28846adde73
gholibeigian <gholibeigian.k85@htlwienwest.at>
2020-12-17 20:58:55 +0100

    Added some persons

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+	// write your code here
+        System.out.println("Here I am going to do it!");
+        Person myPerson = new Person(12,"Sandra","haruiosth23234");
+        Person myPerson1 = new Person(23,"Amanda","haruio34");
+        Person myPerson2 = new Person(25,"Gabriella","asdfuio23");
+        Person myPerson3 = new Person(32,"Fati","th23234");
         // write your code here
 
         Account myAccount = new Account(123,"Start");
