c31a011f4b9afd161f7103fbc3e10b915d4eb332
gholibeigian <gholibeigian.k85@htlwienwest.at>
2020-12-17 21:29:11 +0100

    THe end of the second lab!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,5 +15,11 @@ public class Main {
         Account myAccount = new Account(54,"St");
         Account myAccount = new Account(87945,"Mt");
         Account myAccount = new Account(576,"Zut");
+
+        System.out.println("Hallo Mary, I love" +
+                "you!");
+        System.out.println("How is the weather there");
+        System.out.println("Where is the boss!");
+        System.out.println("Where is the boss!");
     }
 }
